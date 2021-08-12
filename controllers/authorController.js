c0404b151a33b0311fb140a9467dab4dde5e5e59
Nico Nap <nico.nap@outlook.com>
2021-08-12 10:06:45 +0200

    Add updating authors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -127,7 +127,17 @@ exports.author_delete = function(req, res, next) {
 
 exports.author_update = [
   (req, res, next) => {
-    next();
+    jwt.verify(req.token, 'secret', function(err, authData) {
+      if (err) {
+        res.json({
+          error: 403,
+          message: "You do not have permission to update authors"
+        })
+      } else {
+        req.authData = authData;
+        next();
+      }
+    })
   },
 
   body('firstname', 'First name must be longer than 3 characters.').trim().isLength({ min: 3 }).escape(),
@@ -140,7 +150,7 @@ exports.author_update = [
     return new Promise((resolve, reject) => {
       Author.findOne({ username: req.body.username }, function(err, author) {
         if (err) return next(err);
-        if (author && author.username == value) {
+        if (author && author.username == value && author.username != req.authData.username) {
           reject(new Error('Username is already in use.'))
         }
         resolve(true);
@@ -150,26 +160,49 @@ exports.author_update = [
   body('password', 'Password must be longer than 8 characters').isLength({ min: 8 }),
 
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({errors: errors.array()});
-      return;
-    } else {
-      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-        if (err) return next(err);
-        var author = new Author({
-          firstname: req.body.firstname,
-          lastname: req.body.lastname,
-          username: req.body.username,
-          password: hashedPassword,
-        }).save(err => {
+    async.parallel({
+      author: function(callback) {
+        Author.findById(req.params.id).exec(callback);
+      }
+    },
+    function(err, results) {
+      if (err) return next(err);
+      if (results.author.username != req.authData.username) {
+        res.json({
+          error: 403,
+          message: "You do not have permission to update this author."
+        })
+      }
+      const errors = validationResult(req);
+      if (results.author == null) {
+        res.json({
+          error: 404,
+          message: "Author not found"
+        })
+        return;
+      }
+      if (!errors.isEmpty()) {
+        res.json({errors: errors.array()});
+        return;
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
           if (err) return next(err);
-          res.json({
-            message: "Signup complete!",
-            author,
+          var author = new Author({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            password: hashedPassword,
+            _id: req.params.id
+          })
+          Author.findByIdAndUpdate(req.params.id, author, { new: true }, function(err, newAuthor) {
+            if (err) return next(err);
+            res.json({
+              message: "Author succesfully updated!",
+              author: newAuthor
+            })
           })
         })
-      })
-    }
+      }
+    })
   }
 ]
