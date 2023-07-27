5fe5cfab65a5755a8de42bdca92a19aa6174c166
Nico <nico.nap@outlook.com>
2023-07-27 17:45:54 +0200

    Use cookie-session instead of express-session

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var passport = require('passport');
 var compression = require('compression');
 var helmet = require('helmet');
 var cors = require('cors');
-var session = require('express-session');
+const cookieSession = require('cookie-session');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const Author = require('./models/author');
@@ -18,47 +18,49 @@ const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'username',
-      passwordfield: 'password',
-    },
-    function (username, password, done) {
-      Author.findOne({ username: username }, (err, user) => {
-        if (err) {
-          return done(err);
+    new LocalStrategy(
+        {
+            usernameField: 'username',
+            passwordfield: 'password',
+        },
+        function (username, password, done) {
+            Author.findOne({ username: username }, (err, user) => {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username' });
+                }
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (res) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, {
+                            message: 'Incorrect password',
+                        });
+                    }
+                });
+            });
         }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username' });
-        }
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            return done(null, user);
-          } else {
-            return done(null, false, { message: 'Incorrect password' });
-          }
-        });
-      });
-    }
-  )
+    )
 );
 
 passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
-    },
-    function (jwtPayload, callback) {
-      return Author.findOneById(jwtPayload.id)
-        .then((user) => {
-          return callback(null, user);
-        })
-        .catch((err) => {
-          return callback(err);
-        });
-    }
-  )
+    new JWTStrategy(
+        {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'secret',
+        },
+        function (jwtPayload, callback) {
+            return Author.findOneById(jwtPayload.id)
+                .then((user) => {
+                    return callback(null, user);
+                })
+                .catch((err) => {
+                    return callback(err);
+                });
+        }
+    )
 );
 
 var blogRouter = require('./routes/blog');
@@ -66,11 +68,10 @@ var authRouter = require('./routes/auth');
 
 var app = express();
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
-    resave: false,
-  })
+    cookieSession({
+        name: 'session',
+        keys: [process.env.SESSION_SECRET],
+    })
 );
 app.use(cors());
 app.use(compression());
@@ -83,12 +84,12 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
 });
 
 // view engine setup
@@ -108,26 +109,26 @@ app.use(express.static(path.join(__dirname, 'userfrontend')));
 app.use('/blog', blogRouter);
 app.use('/auth', authRouter);
 app.use('/author-frontend', (req, res) => {
-  res.sendFile(path.join(__dirname, 'authorfrontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'authorfrontend', 'index.html'));
 });
 app.use('/blog-frontend', (req, res) => {
-  res.sendFile(path.join(__dirname, 'userfrontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'userfrontend', 'index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err, message: err.message });
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err, message: err.message });
 });
 
 module.exports = app;
